f1536168f8af45757109aeb73faf155de76f86b7
wilmar <wil.pac510@gmail.com>
2021-09-30 23:44:19 -0500

    Mix Prototype and Singleton

--- src/main/java/com/wussines/tutoriales/scope/PersonaDao.java
+++ src/main/java/com/wussines/tutoriales/scope/PersonaDao.java
@@ -1,12 +1,9 @@
 package com.wussines.tutoriales.scope;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PersonaDao {
 	
 	@Autowired
